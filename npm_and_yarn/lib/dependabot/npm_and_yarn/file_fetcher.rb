53bac3b1953564625138f58a8e5609189963c018
David Rodríguez <deivid.rodriguez@riseup.net>
2023-04-19 18:36:40 +0200

    Fix yarn version memoization
    It was checked, but not set.

--- npm_and_yarn/lib/dependabot/npm_and_yarn/file_fetcher.rb
+++ npm_and_yarn/lib/dependabot/npm_and_yarn/file_fetcher.rb
@@ -136,7 +136,7 @@ module Dependabot
       def yarn_version
         return @yarn_version if defined?(@yarn_version)
 
-        package_manager.locked_version("yarn") || guess_yarn_version
+        @yarn_version = package_manager.locked_version("yarn") || guess_yarn_version
       end
 
       def guess_yarn_version
